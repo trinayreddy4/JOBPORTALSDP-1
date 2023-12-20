c9ffbf0f42972aaaea0b0bc4e3da9d8261594f7f
trinayreddy4 <trinayreddy4@gmail.com>
2023-12-20 14:47:37 +0530

    Multer Addition

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const UserControllers=require('../backend/Controllers/UserControllers')
 const port=process.env.PORT||5000;
 const resumeUpload=require('./Controllers/ResumeUpload');
 
+{process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_MODE : process.env.REACT_APP_PRO_MODE}
 app.use(cors());
 app.use(express.json());
 mongoose.connect('mongodb://127.0.0.1:27017/sdp-2_jobportal').then(()=>console.log('Connected to MongoDB')).catch(err=>console.log(err));
